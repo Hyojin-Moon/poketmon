874e0ef72a05113ee8d9c3572e9843ff606015ab
Hyojin-Moon <moontough8@gmail.com>
2025-02-10 14:21:05 +0900

    📝 Add: store 생성

--- src/redux/slices/pokemonSlice.js
+++ src/redux/slices/pokemonSlice.js
@@ -13,16 +13,16 @@ const pokemonSlice = createSlice({
   reducers: {
     addPokemon: (state, action) => {
       const pokemon = action.payload;
-      if (state.selectedPokemons.length < 6 && !state.selectedPokemons.some(p => p.id === pokemon.id)) {
-        state.setSelectedPokemons.push(pokemon);
-      } else if (state.selectedPokemons.some(p => p.id === pokemon.id)) {
+      if (state.selectiedPokemons.length < 6 && !state.selectiedPokemons.some(p => p.id === pokemon.id)) {
+        state.selectiedPokemons.push(pokemon);
+      } else if (state.selectiedPokemons.some(p => p.id === pokemon.id)) {
         alert("이미 잡은 포켓몬 입니다!")
       } else {
         alert("최대 6마리까지만 잡을 수 있습니다!");
       }
     },
     removePokemon: (state, action) => {
-      state.selectiedPokemons = state.selectedPokemons.filter(pokemon => pokemon.id !== action.payload)
+      state.selectiedPokemons = state.selectiedPokemons.filter(pokemon => pokemon.id !== action.payload)
     },
     setPokemonData: (state, action) => {
       state.pokemonData = action.payload;
